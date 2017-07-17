52e7c3592ff0b2f5b2b9e654937703c9d545958d
rusth886 <rusth.cn@gmail.com>
2017-07-17 14:11:00 +0800

    优化

--- src/main/java/cn/rusth/RusthSpringbootSsmApplication.java
+++ src/main/java/cn/rusth/RusthSpringbootSsmApplication.java
@@ -3,6 +3,9 @@ package cn.rusth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 运行主方法，使用此方法启动项目，自动加载tomcat
+ */
 @SpringBootApplication
 public class RusthSpringbootSsmApplication {
 
